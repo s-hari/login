7b0e4e8e7da69f4e6723e031449dbc81203cdfb4
s-hari <harishankarsahu123@gmail.com>
2016-08-31 18:58:28 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,15 @@
+import  java.awt.*;
+import  java.awt.event.*;
+public  class  bank  extends  Frame 
+{
+
+  TextField  t1;
+bank()
+{ setLayout(new FlowLayout());
+   t1=new TextField(10);
+
+ add(t1);
+
+}
+}
+ 
